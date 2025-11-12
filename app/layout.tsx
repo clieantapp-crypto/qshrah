6af4f549f3510a1c2a490483a38a1ce995bc05c5
clieantapp-crypto <clieantapp@gmail.com>
2025-11-12 09:28:23 +0300

    asdc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Cairo, Noto_Sans_Arabic } from "next/font/google"
 import "./globals.css"
+import Script from "next/script"
 
 
 export const metadata: Metadata = {
@@ -34,6 +35,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
+         <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5J3M1H0VBN" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5J3M1H0VBN');
+          `}
+        </Script>
+      </head>
       <body >
         {children}
       </body>
